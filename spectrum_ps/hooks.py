317e78ae8457361be0050cb2643af6963e80852d
Narayan Bandodker <narayan.b@phaidelta.com>
2026-09-24 15:58:51 +0530

    fix doc event path

--- spectrum_ps/hooks.py
+++ spectrum_ps/hooks.py
@@ -154,9 +154,9 @@ app_license = "mit"
 
 doc_events = {
     "Customer": {
-		"after_insert": "spectrum_ps.handler.customer.create_customer_bot",
-		"on_update": "spectrum_ps.handler.customer.create_customer_bot",
-		"on_trash": "spectrum_ps.handler.customer.remove_customer_bot",
+		"after_insert": "spectrum_ps.spectrum_ps.doc_events.customer.create_customer_bot",
+		"on_update": "spectrum_ps.spectrum_ps.doc_events.customer.create_customer_bot",
+		"on_trash": "spectrum_ps.spectrum_ps.doc_events.customer.remove_customer_bot",
 	}
 }
 
