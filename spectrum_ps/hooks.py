433ba97aae3f6284462caa5cdc752184294c4e22
Narayan Bandodker <narayan.b@phaidelta.com>
2026-09-24 14:31:39 +0530

    raven bot create and remove hooks

--- spectrum_ps/hooks.py
+++ spectrum_ps/hooks.py
@@ -152,6 +152,14 @@ app_license = "mit"
 # 	}
 # }
 
+doc_events = {
+    "Customer": {
+		"after_insert": "spectrum_ps.handler.customer.create_customer_bot",
+		"on_update": "spectrum_ps.handler.customer.create_customer_bot",
+		"on_trash": "spectrum_ps.handler.customer.remove_customer_bot",
+	}
+}
+
 # Scheduled Tasks
 # ---------------
 
---
+++ spectrum_ps/spectrum_ps/handler/customer.py
@@ -0,0 +1,40 @@
+import frappe
+from frappe import _
+
+
+def _get_bot_id(doc: "Customer"):
+	return f"customer - {doc.name}"
+
+
+def create_customer_bot(doc: "Customer", method):
+	"""called when the customer is inserted or updated"""
+
+	# If the customer is already added to Raven Bot, do nothing.
+	bot_id = _get_bot_id(doc)
+
+	if frappe.db.exists("Raven Bot", {"name": bot_id}):
+		# Check if this Customer has been linked to the bot
+		if doc.custom_raven_bot != bot_id:
+			frappe.log("Assigning Existing bot account `%s` to customer `%s`" % (bot_id, doc.name))
+			doc.custom_raven_bot = bot_id
+	else:
+		# Raven bot does not exist.
+		# Only create raven bot if it exists in the system.
+		if frappe.db.exists("Customer", doc.name):
+			# Create a Raven Bot record for the customer.
+			raven_bot = frappe.new_doc("Raven Bot")
+			raven_bot.name = bot_id
+			raven_bot.bot_name = doc.customer_name
+			raven_bot.insert(ignore_permissions=True)
+			doc.custom_raven_bot = bot_id
+
+
+def remove_customer_bot(doc: "Customer", method):
+	"""called when the customer is deleted"""
+
+	bot_id = _get_bot_id(doc)
+
+	# If the customer is deleted, then delete the Raven Bot record for the customer.
+	if frappe.db.exists("Raven Bot", {"name": bot_id}):
+		raven_bot = frappe.get_doc("Raven Bot", {"name": bot_id})
+		raven_bot.delete(ignore_permissions=True)
