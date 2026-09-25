8e773e1039c0707f28ce48be0a1d153a8c0078e7
Narayan Bandodker <narayan.b@phaidelta.com>
2026-09-25 14:19:37 +0530

    added logging and exceptions on error cases

--- spectrum_ps/spectrum_ps/doc_events/customer.py
+++ spectrum_ps/spectrum_ps/doc_events/customer.py
@@ -9,27 +9,29 @@ def _get_bot_id(doc):
 def create_customer_bot(doc, method):
 	"""called when the customer is inserted or updated"""
 
-	# If the customer is already added to Raven Bot, do nothing.
 	bot_id = _get_bot_id(doc)
+	frappe.log(f"Ensuring Raven Bot `{bot_id}` exists for Customer `{doc.name}`")
 
 	if frappe.db.exists("Raven Bot", {"name": bot_id}):
-		# Check if this Customer has been linked to the bot
 		if doc.custom_raven_bot != bot_id:
-			frappe.log("Assigning Existing bot account `%s` to customer `%s`" % (bot_id, doc.name))
+			frappe.log(f"Linking existing Raven Bot `{bot_id}` to Customer `{doc.name}`")
 			doc.custom_raven_bot = bot_id
 			doc.save(ignore_permissions=True)
 			sync_customer_raven_user(frappe.get_doc("Raven Bot", bot_id))
+		else:
+			frappe.log(f"Customer `{doc.name}` is already linked to Raven Bot `{bot_id}`")
 	else:
-		# Raven bot does not exist.
-		# Only create raven bot if it exists in the system.
 		if frappe.db.exists("Customer", doc.name):
-			# Create a Raven Bot record for the customer.
+			frappe.log(f"Creating Raven Bot `{bot_id}` for Customer `{doc.name}`")
 			raven_bot = frappe.new_doc("Raven Bot")
 			raven_bot.bot_name = bot_id
 			raven_bot.insert(ignore_permissions=True)
 			doc.custom_raven_bot = bot_id
 			doc.save(ignore_permissions=True)
 			sync_customer_raven_user(raven_bot)
+		else:
+			frappe.log(f"Customer `{doc.name}` does not exist while creating its Raven Bot")
+			frappe.throw(_("Customer {0} must exist before its Raven Bot can be created.").format(doc.name))
 
 
 def sync_customer_raven_user(doc, method=None):
@@ -41,12 +43,15 @@ def sync_customer_raven_user(doc, method=None):
 		as_dict=True,
 	)
 	if not customer:
+		frappe.log(f"Raven Bot `{doc.name}` is not linked to a Customer")
 		return
 
 	raven_user_name = frappe.db.get_value("Raven Bot", doc.name, "raven_user")
 	if not raven_user_name:
-		return
+		frappe.log(f"Raven Bot `{doc.name}` has no linked Raven User")
+		frappe.throw(_("Raven Bot {0} must have a linked Raven User.").format(doc.name))
 
+	frappe.log(f"Synchronizing Raven User `{raven_user_name}` for Customer `{customer.customer_name}`")
 	raven_user = frappe.get_doc("Raven User", raven_user_name)
 	if raven_user.full_name == customer.customer_name and raven_user.first_name == customer.first_name:
 		return
@@ -54,6 +59,7 @@ def sync_customer_raven_user(doc, method=None):
 	raven_user.full_name = customer.customer_name
 	raven_user.first_name = customer.first_name
 	raven_user.save(ignore_permissions=True)
+	frappe.log(f"Updated Raven User `{raven_user.name}` for Customer `{customer.customer_name}`")
 
 
 def remove_customer_bot(doc, method):
@@ -61,7 +67,9 @@ def remove_customer_bot(doc, method):
 
 	bot_id = _get_bot_id(doc)
 
-	# If the customer is deleted, then delete the Raven Bot record for the customer.
 	if frappe.db.exists("Raven Bot", {"name": bot_id}):
+		frappe.log(f"Removing Raven Bot `{bot_id}` for deleted Customer `{doc.name}`")
 		raven_bot = frappe.get_doc("Raven Bot", {"name": bot_id})
 		raven_bot.delete(ignore_permissions=True)
+	else:
+		frappe.log(f"No Raven Bot found to remove for deleted Customer `{doc.name}`")
