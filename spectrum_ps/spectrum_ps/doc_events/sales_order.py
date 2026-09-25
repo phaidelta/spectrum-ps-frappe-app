f69af10b81a700d5e44b5d3084ab57bef7b1b17d
Narayan Bandodker <narayan.b@phaidelta.com>
2026-09-25 10:54:32 +0530

    fix sub-call permission issue

--- spectrum_ps/spectrum_ps/doc_events/sales_order.py
+++ spectrum_ps/spectrum_ps/doc_events/sales_order.py
@@ -32,7 +32,12 @@ def create_raven_channel(doc: "Sales Order", method=None):
 		return
 
 	bot_user = frappe.get_cached_doc("Raven Bot", customer.custom_raven_bot)
-	bot_user.add_to_channel(raven_channel.name)
+	previous_ignore_permissions = getattr(frappe.flags, "ignore_permissions", False)
+	frappe.flags.ignore_permissions = True
+	try:
+		bot_user.add_to_channel(raven_channel.name)
+	finally:
+		frappe.flags.ignore_permissions = previous_ignore_permissions
 
 
 def remove_raven_channel(doc: "Sales Order", method=None):
